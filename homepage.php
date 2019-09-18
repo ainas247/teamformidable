9293115438041d20866e7cf581c1eb93f2bc720d
Andy Gabriel <54823365+andygcodes@users.noreply.github.com>
2019-09-18 20:39:35 +0100

    Update homepage.php
    User's access right should be checked before viewing the page.

--- homepage.php
+++ homepage.php
@@ -1,2 +1,6 @@
+<?php
+// if user is not logged on, redirect to login page
+?>
+
 <h1>Welcome</h1> <?php echo $_SESSION['user']; ?>
 <p><a href="logout.php?logout='1'"><b><i class="fa fa-power-off" aria-hidden="true"></i>logout</b></a></b></p>
