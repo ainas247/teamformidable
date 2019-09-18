ae79dac084907f0efdfb3a2b75838420d1accfc0
Eze-ThankGod <54280472+Eze-ThankGod@users.noreply.github.com>
2019-09-18 16:10:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
