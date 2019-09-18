46cbbafaf3b4a1252637ca2c359bac3a5f7e74b5
Achilles <achillesusuoyibo7@gmail.com>
2019-09-18 11:31:09 +0100

    Added signup form and form toggle functionalities

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // The Error Message that will dsiplay under the form
 document.getElementById("username_error_message").style.display = "none";
 document.getElementById("password_error_message").style.display = "none";
-
+document.querySelector('.register__form').style.display = 'none';
 // Getting the button
 let btn = document.getElementById("btn");
 btn.addEventListener("click", validateLogin);
@@ -24,4 +24,31 @@ function validateLogin(e) {
   } else {
     alert("login");
   }
+}
+
+
+
+//Function to Togl Between Register and Login Forms
+//Toggle to show the Register form
+toggleSignup = function(e){
+  const loginBtn  = document.querySelector('.register__link');
+  const loginForm = document.querySelector('.login__form');
+  const signUpForm = document.querySelector('.register__form');
+  loginBtn.addEventListener('click', ()=>{
+    loginForm.style.display = 'none';
+    signUpForm.style.display = 'block';
+  })
+  e.preventDefault();
+}
+
+//Toggle to show the Login Form
+toggleLogin = function(e){
+  const loginBtn  = document.querySelector('.register__link');
+  const loginForm = document.querySelector('.login__form');
+  const signUpForm = document.querySelector('.register__form');
+  loginBtn.addEventListener('click', ()=>{
+    loginForm.style.display = 'block';
+    signUpForm.style.display = 'none';
+  })
+  e.preventDefault();
 }
